cd43379261fe409b7e16d419a9197513fe853c96
Bryan PC <bryanfox1986@gmail.com>
2018-12-25 20:53:56 -0500

    layout starting

--- src/Spinner.js
+++ src/Spinner.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Loader, Dimmer } from "semantic-ui-react";
 
 const Spinner = () => (
-  <Dimmer>
+  <Dimmer active>
     <Loader size="huge" content={"Preparing Chat..."} />
   </Dimmer>
 );
