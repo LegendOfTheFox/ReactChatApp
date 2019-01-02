156573826fb471b6aa2bfe874bc1403f19b0ec3f
Bryan PC <bryanfox1986@gmail.com>
2019-01-01 19:12:26 -0500

    added main chat menu system

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,8 +1,112 @@
 import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import firebase from "../../firebase";
+
+import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessageForm";
+import Message from "./Message";
 
 class Messages extends React.Component {
+  state = {
+    messagesRef: firebase.database().ref("messages"),
+    messages: [],
+    messagesLoading: true,
+    channel: this.props.currentChannel,
+    user: this.props.currentUser,
+    progressBar: false,
+    numUniqueUsers: ""
+  };
+
+  componentDidMount() {
+    const { channel, user } = this.state;
+
+    if (channel && user) {
+      this.addListeners(channel.id);
+    }
+  }
+
+  addListeners = channelId => {
+    this.addMessageListener(channelId);
+  };
+
+  addMessageListener = channelId => {
+    let loadedMessages = [];
+    this.state.messagesRef.child(channelId).on("child_added", snap => {
+      loadedMessages.push(snap.val());
+      console.log(loadedMessages);
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false
+      });
+      this.countUniqueUser(loadedMessages);
+    });
+  };
+
+  countUniqueUser = messages => {
+    const uniqueUsers = messages.reduce((acc, message) => {
+      if (!acc.includes(message.user.name)) {
+        acc.push(message.user.name);
+      }
+      return acc;
+    }, []);
+    const plural = uniqueUsers.length > 1 || uniqueUsers.length === 0;
+    const numUniqueUsers = `${uniqueUsers.length} user${plural ? "s" : ""}`;
+    this.setState({ numUniqueUsers });
+  };
+
+  displayMessages = messages =>
+    messages.length > 0 &&
+    messages.map(message => (
+      <Message
+        key={message.timestamp}
+        message={message}
+        user={this.state.user}
+      />
+    ));
+
+  isProgressBarVisible = percent => {
+    if (percent > 0) {
+      this.setState({
+        progressBar: true
+      });
+    }
+  };
+
+  displayChannelName = channel => (channel ? `#${channel.name}` : "");
+
   render() {
-    return <div>Messages</div>;
+    const {
+      messagesRef,
+      messages,
+      channel,
+      user,
+      progressBar,
+      numUniqueUsers
+    } = this.state;
+
+    return (
+      <React.Fragment>
+        <MessagesHeader
+          channelName={this.displayChannelName(channel)}
+          numUniqueUsers={numUniqueUsers}
+        />
+
+        <Segment>
+          <Comment.Group
+            className={progressBar ? "messages__progress" : "messages"}
+          >
+            {this.displayMessages(messages)}
+          </Comment.Group>
+        </Segment>
+
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+          isProgressBarVisible={this.isProgressBarVisible}
+        />
+      </React.Fragment>
+    );
   }
 }
 
